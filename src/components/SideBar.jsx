42d12fc43ce65b8d852a7c709feb56efabd279f2
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-12-07 21:01:03 -0800

    change SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,54 +1,66 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import React, { useState, useEffect, useRef } from 'react';
 import AddProjectPopup from './AddProjectPopup.jsx';
 
-function SideBar() {
+function SideBar({projects, addProject, setActivePrjId}) {
+  /*
   const [projects, setProjects] = useState([
     {title: 'text Susan', description: 'ask about Renee Cosmo', id: _.uniqueId()},
     {title: 'wipe cat shelves',  description: 'wipe shleves w solution', id: _.uniqueId()},
   ]);
+  */
   // const [isAddProjectPopupOpen, setIsAddProjectPopupOpen] = useState(false);
 
   const btnElem = useRef(null);
   const popupElem = useRef(null);
-  const prjID = useRef(null);
 
 
   const generateProjectsList = () => {
-    const liElems = projects.map(({title, id}) => <li key={id} id={id}><button type="button">{title}</button></li>);
+    const liElems = projects.map(({title, id}) => <li key={id} id={id}>
+      <button type="button" onClick={() => {
+        console.log(id)
+        setActivePrjId(id);
+      }}
+      >{title}</button>
+    </li>);
     return <ul className="mt-8">{liElems}</ul>
   };
 
   const handleAddProjectBtnClick = () => {
     // setIsAddProjectPopupOpen(true);
-    btnElem.current.disabled = true;
-    popupElem.current.showModal(); // API
+    // btnElem.current.disabled = true;
+    popupElem.current.openModal(); // API
   };
 
 
+  // MOVE TO POPUP
+  /*
+  const closeAddProjectPopup = () => {
+    popupElem.current.close(); // API
+    // btnElem.current.disabled = false;
+  };
+  */
+
+  /*
   const addProject = (prj) => {
     setProjects((prev) => [...prev, prj]);
   };
+  */
 
-
-  const closeAddProjectPopup = () => {
-    popupElem.current.close(); // API
-    btnElem.current.disabled = false;
-  };
-
+  // MOVE TO POPUP
+  /*
   const handleDialogueSubmit = (e) => {
     closeAddProjectPopup();
     const formData = new FormData(e.target);
     const formDataObj = Object.fromEntries(formData);
-    // prjID.current += 1;
     addProject({...formDataObj, id: _.uniqueId()});
   };
-
+  */
 
   
   return (
     <>
-      <AddProjectPopup ref={popupElem} onPopupSubmit={handleDialogueSubmit} onPopupClose={closeAddProjectPopup} />
+      <AddProjectPopup ref={popupElem} addProject={addProject} />
       <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl rounded-b-none">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
         <button
@@ -66,3 +78,5 @@ function SideBar() {
 }
 
 export default SideBar;
+
+// <AddProjectPopup ref={popupElem} onPopupSubmit={handleDialogueSubmit} onPopupClose={closeAddProjectPopup} />
