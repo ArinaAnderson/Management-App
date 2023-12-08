6e4cb51767b2e52e076e9029a8e0b712428beba7
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-12-07 21:01:23 -0800

    change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import _ from 'lodash';
 import React, { useState, useEffect, useRef } from 'react';
-import Project from './components/Project.jsx';
+// import Project from './components/Project.jsx';
 import SideBar from './components/SideBar.jsx';
+import Main from './components/Main.jsx';
 
 function App() {
-  // const [projects, setProjects] = useState([{title: 'text Susan', id: 0}, {title: 'wipe cat shelves', id: 1}]);
-  /*
-  const generateProjectsList = () => {
-    const liElems = projects.map(({title, id}) => <li key={id}>{title}</li>);
-    return <ul className="mt-8">{liElems}</ul>
+  const [projects, setProjects] = useState([
+    /*
+    {title: 'text Susan', description: 'ask about Renee Cosmo', id: _.uniqueId()},
+    {title: 'wipe cat shelves',description: 'ask about Renee Cosmo', id: _.uniqueId()},
+    */
+  ]);
+  const [activePrjId, setActivePrjId] = useState(null);
+
+  const addProject = (prj) => {
+    setProjects((prev) => [...prev, prj]);
   };
-  */
-  const [activeProject, setActiveProject] = useState(null);
-  
+
   return (
     <>
       
       <main className="h-screen my-8 flex gap-8">
-        <SideBar setActiveProject={setActiveProject}/>
-        <Project />
+        <SideBar projects={projects} setActivePrjId={setActivePrjId} addProject={addProject} />
+        <Main projects={projects} activePrjId={activePrjId} addProject={addProject} />
       </main>
       
 
