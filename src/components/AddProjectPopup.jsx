d9daa1030fbd541ce95ed6ed391a5c9489ed1edd
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-12-11 21:22:38 -0800

    switch from dialog and  to div with input and labels

--- src/components/AddProjectPopup.jsx
+++ src/components/AddProjectPopup.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import _ from 'lodash';
 import { forwardRef, useImperativeHandle } from 'react';
+import Input from './Input.jsx';
 
 const AddProjectPopup = forwardRef(({addProject}, ref) => {
   const dialogElem = useRef(null);
 
   useImperativeHandle(ref, () => ({
     openModal() {
-      dialogElem.current.showModal();
+      // dialogElem.current.showModal();
     }
   }));
 
@@ -24,35 +25,22 @@ const AddProjectPopup = forwardRef(({addProject}, ref) => {
   };
 
 
-  return (
-    <dialog ref={dialogElem} onClose={(e) => console.log('MARMUUUUU', e.type)} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
-      <form className="mt-4 text-right" method="dialog" onSubmit={handleDialogueSubmit}>
-        <menu className="flex items-center justify-end gap-4 my-4">
-          <li><button onClick={closeAddProjectPopup} type="button" className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Cancel</button></li>
-          <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
-        </menu>
-        <label htmlFor="title" className="text-sm font-bold uppercase text-stone-500">
-          Title
-        </label>
-        <input
-          id="title"
-          type="text"
-          name="title"
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-        />
-
-        <label htmlFor="description" className="text-sm font-bold uppercase text-stone-500">
-          Description
-        </label>
-        <input
-          id="description"
-          type="text"
-          name="description"
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-        />
-      </form>
-    </dialog>
-  );
+  return  <div ref={dialogElem} className="w-[35rem] mt-16">
+    <menu className="flex items-center justify-end gap-4 my-4">
+      <li><button className="text-stone-800 hover:text-stone-950 hover:underline">Cancel</button></li>
+      <li>
+        <button
+          className="px-6 py-2 border-2 border-stone-800 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-100  hover:text-stone-800">
+          Save
+        </button>
+      </li>
+    </menu>
+    <div>
+      <Input htmlFor='title' label='Title' type='text' textarea={false} id='title' name='title' />
+      <Input htmlFor='description' label='Description' type='text' textarea={true} id='description' name='description' />
+      <Input htmlFor='due-date' label='Due Date' type='text' textarea={false} id='due-date' name='due-date' />
+    </div>
+  </div>
 })
 
 export default AddProjectPopup;
@@ -85,5 +73,41 @@ onSubmit={(e) => {
   console.log('KUKU', formDataObj.title, formDataObj.description);
   props.onPopupClose();
 }}>
+*/
+
+
+
+/*
+
+(
+    <dialog ref={dialogElem} onClose={(e) => console.log('MARMUUUUU', e.type)} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+      <form className="mt-4 text-right" method="dialog" onSubmit={handleDialogueSubmit}>
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li><button onClick={closeAddProjectPopup} type="button" className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Cancel</button></li>
+          <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+        </menu>
+        <label htmlFor="title" className="text-sm font-bold uppercase text-stone-500">
+          Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+
+        <label htmlFor="description" className="text-sm font-bold uppercase text-stone-500">
+          Description
+        </label>
+        <input
+          id="description"
+          type="text"
+          name="description"
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+      </form>
+    </dialog>
+  );
+
 */
 
