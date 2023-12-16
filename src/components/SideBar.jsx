d402290740f78152fdf80c09e4091d7bff0f2783
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-12-15 16:48:26 -0800

    add rendering of projects

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,18 @@
 import React, { useRef } from 'react';
 import Button from './Button.jsx';
 
-function SideBar({ changeCurrentAction }) {
+function SideBar({ projects, changeCurrentAction }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl rounded-b-none">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
       <div>
-        <Button onClick={() =>  changeCurrentAction('add-project')}>
+        <Button onClick={() => changeCurrentAction('add-project')}>
         + Add Project
         </Button>
       </div>
-      <ul></ul>
+      <ul>
+        {projects.map(({title, id}) => <li key={id}><button>{title}</button></li>)}
+      </ul>
     </aside>
   );
 }
