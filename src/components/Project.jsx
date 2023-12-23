3733aece27083b94e99a8c526ef1e5616e264b18
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-12-22 21:15:56 -0800

    modify component Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 
-function Project({activePrjId, projects, deleteProject, setActivePrjId}) {
+function Project({activePrjId, projects, deleteProject}) {
   const [tasks, setTasks] = useState([]);
 
   const activeProject = projects.find((prj) => prj.id === activePrjId);
 
+  const formattedDate = new Date(activeProject.date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold text-stone-600 mb-2">{activeProject.title}</h1>
-        <button onClick={() => {
-          setActivePrjId(null)
-          deleteProject(activeProject);
-        }} className="text-stone-600 hover:text-stone-950">Delete</button>
-      </div>
-      <p className="text-stone-600">{activeProject.description}</p>
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-2 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">{activeProject.title}</h1>
+          <button onClick={() => {
+            deleteProject();
+          }} className="text-stone-600 hover:text-stone-950">
+            Delete
+          </button>
+        </div>
+        <p className="mb-4 text-stone-600">{formattedDate}</p>
+        <p className="text-stone-800 whitespace-pre-wrap">{activeProject.description}</p>
+      </header>
       <h2>Tasks</h2>
       <div className="flex items-center gap-4">
         <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
@@ -26,9 +36,9 @@ function Project({activePrjId, projects, deleteProject, setActivePrjId}) {
 
         </ul>
       )}
-    </>
+    </div>
   );
-}
+}  
 
 export default Project;
 
